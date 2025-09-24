562a7610d48ef69d05a521d2f1f7d7bbd7cbf8f7
BigFatDuck1 <clementho999@gmail.com>
2025-09-25 02:02:30 +0800

    Rework AnswerText.js logic, deleted all logic and starting fresh

--- src/components/AnswerText.jsx
+++ src/components/AnswerText.jsx
@@ -1,39 +1,18 @@
-import React from "react";
-import { useState } from "react";
+import React, { use } from "react";
+import { useRef, useState } from "react";
 
 function AnswerText({ answered, correct_answer, score }) {
 
-    const [prev_score, setPrevScore] = useState(score[0]);
-    let isCorrect; //true/false
 
-    let scoreChange = () => {
-        let return_value;
-        if (score[0] > prev_score) { //User answered correctly
-            return_value =  true;
-        }
-        else if (score[0] == prev_score) { //User answered incorrectly
-            return_value = false;
-        }
-
-        setPrevScore(score[0]);
-        console.log(return_value)
-        return return_value;
-    }
-
-    if (prev_score < score[0]) { //Updates whenever the score changes
-        isCorrect = scoreChange();
-    }
- 
     if (answered == false) {
         return (
             <div className="answer_text" />        
         )
     }
     else if (answered == true) {
-        console.log(prev_score, ",", score[0], ",", isCorrect);
         return (
             <div className="answer_text">
-                {isCorrect ? "Correct!" : "Incorrect."} The correct answer is: {correct_answer}
+                {1 ? "Correct!" : "Incorrect."} The correct answer is: {correct_answer}
             </div>
         )
     }
