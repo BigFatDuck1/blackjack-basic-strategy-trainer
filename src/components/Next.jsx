fc7067608282a662c82710ad86681b9173017496
Clement <clementho999@gmail.com>
2025-10-04 01:49:43 +0800

    Added hole card animation; added tooltip when answer not selected but next button is pressed

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Random from "./Random.jsx";
+import { Tooltip } from 'primereact/tooltip';
+import { Button } from 'primereact/button';
 
-function Next({ toggle_answer, score, setScore, setRandom, setAnswer }) {
+
+
+
+function Next({ toggle_answer, score, setScore, setRandom, setAnswer, setCorrect_state }) {
 
     const answered = toggle_answer[0]; //true means user has submmited answer, false means user hasn't submitted an answer
     const setAnswered = toggle_answer[1];
@@ -9,7 +14,7 @@ function Next({ toggle_answer, score, setScore, setRandom, setAnswer }) {
     const reset = () => {
         if (answered == false) {
             //User has not submitted any answer yet
-            alert("Please select an answer");
+            //alert("Please select an answer");
             return;
         }
 
@@ -18,13 +23,19 @@ function Next({ toggle_answer, score, setScore, setRandom, setAnswer }) {
         const newPair = Random();
         setRandom(newPair); // Generate new dealer/player pair
         setAnswer(newPair.answer); // Set correct answer to the correct move for the new dealer/player pair
+        setCorrect_state(null); //Reset correct_state to null for new question
 
     }
 
 
     return (
+        // <div className="next">
+        //     <button className="next_button" onClick={reset} data-pr-tooltip="Please select an answer" >Next</button>
+        // </div>
         <div className="next">
-            <button className="next_button" onClick={reset} >Next</button>
+            <Button label="Next" className="next_button" onClick={reset} 
+            tooltip={answered ? "" : "Please select an answer"} 
+            tooltipOptions={{ position: 'bottom' }} />
         </div>
     )
 }
