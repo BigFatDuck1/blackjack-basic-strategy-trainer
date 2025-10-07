4481dec69b4db91defd15628f498478090d88cdd
Clement <clementho999@gmail.com>
2025-10-07 16:45:12 +0800

    Updated Hit/Stand with all questions (see README.md)

--- src/components/HitStand.jsx
+++ src/components/HitStand.jsx
@@ -9,6 +9,25 @@ const suites = ["♥", "♦", "♠", "♣"];
 
 const all_array = [
     [9, 6, 8, "H"],
+    [9, 7, 5, "S"],
+    [7, 5, 3, "H"],
+    [7, 2, 10, "H"],
+    [6, 3, 7, "H"],
+    [9, 3, 3, "H"],
+    [10, 7, 4, "S"],
+    [9, 3, 5, "S"],
+    [10, 2, 3, "H"],
+    [10, 2, 2, "H"],
+    [10, 3, 3, "S"],
+    [10, 3, 4, "S"],
+    [10, 7, 2, "S"],
+    [10, 5, 7, "H"],
+    [10, 6, 9, "H"],
+    [10, 6, 10, "H"],
+    [7, 6, 3, "S"],
+    [8, 7, 9, "H"],
+    [9, 7, 10, "H"],
+    [10, 4, 2, "S"]
 ]
 
 function parseArray (big_array) {
@@ -36,12 +55,13 @@ function parseArray (big_array) {
     card2 = tenRandomizer(card2);
     card3 = tenRandomizer(card3);
 
-    return obj = {
+    obj = {
         player_card1_string: `${card1suit} ${card1}`,
         player_card2_string: `${card2suit} ${card2}`,
         dealer_card_string: `${card3suit} ${card3}`,
         answer: `${array[3] == "H" ? "Hit" : "Stand"}`,
     }
+    return obj;
 }
 
 
