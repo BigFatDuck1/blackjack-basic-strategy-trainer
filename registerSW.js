819462d4bdd2fbcff14dbc8c95edc2c25ea948b3
Clement <clementho999@gmail.com>
2025-10-07 16:52:32 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/blackjack-basic-strategy-trainer/sw.js', { scope: '/blackjack-basic-strategy-trainer/' })})}
